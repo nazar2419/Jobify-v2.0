ee7e5b4f5e64000b77fa1fa96a50e502c19776dd
Nazar <nazarx095@gmail.com>
2023-09-26 16:03:33 +0300

    landing page styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,42 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  HomeLayout,
+  Landing,
+  Register,
+  Login,
+  DashboardLayout,
+  Error,
+  AddJob,
+  Stats,
+  AllJobs,
+  Profile,
+  Admin,
+  EditJob,
+} from "./pages";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
+
 const App = () => {
-  return <h1>Jobify</h1>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
---
+++ client/src/pages/Landing.jsx
@@ -0,0 +1,34 @@
+import Wrapper from "../assets/wrappers/LandingPage";
+import main from "../assets/images/main.svg";
+import { Link } from "react-router-dom";
+import { Logo } from "../components";
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque,
+            omnis impedit. Dolorum dolor eaque quisquam! Ipsum enim ducimus
+            odit, voluptate facilis reiciendis fugiat molestiae adipisci atque
+            tenetur, hic eaque omnis.
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn">
+            Login
+          </Link>
+        </div>
+        <img src={main} alt="job" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+export default Landing;
