db898a8f090c3ae72360adc175095dfdc01c3aa4
Nazar <nazarx095@gmail.com>
2023-10-18 14:10:11 +0300

    get and edit job

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.post("/", (req, res) => {
   console.log(req);
   res.json({ message: "data received", data: req.body });
 });
-//GET JOBS
+//GET All JOBS
 app.get("/api/v1/jobs", (req, res) => {
   res.status(200).json({ jobs });
 });
@@ -41,6 +41,33 @@ app.post("/api/v1/jobs", (req, res) => {
   const job = { id, company, position };
   jobs.push(job);
   res.status(200).json({ job });
+});
+
+// GET SINGLE JOB
+app.get("/api/v1/jobs/:id", (req, res) => {
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+  res.status(200).json({ job });
+});
+
+// EDIT JOB
+app.patch("/api/v1/jobs/:id", (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res.status(400).json({ msg: "please provide company and position" });
+  }
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id #{id}` });
+  }
+  job.company = company;
+  job.position = position;
+
+  res.status(200).json({ msg: `job modified`, job });
 });
 
 const port = process.env.PORT || 5100;
