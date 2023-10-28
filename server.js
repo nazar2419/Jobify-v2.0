2b3ef7a9ad4d70b1bdc06b300049f74427d3c925
Nazar <nazarx095@gmail.com>
2023-10-28 13:20:30 +0300

    job model and create job controller

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ dotenv.config();
 import express from "express";
 const app = express();
 import morgan from "morgan";
-import { nanoid } from "nanoid";
+import mongoose from "mongoose";
 
-let jobs = [
-  { id: nanoid(), company: "apple", position: "front-end" },
-  { id: nanoid(), company: "google", position: "back-end" },
-];
+//routers
+import jobRouter from "./routers/jobRouter.js";
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -26,52 +24,25 @@ app.post("/", (req, res) => {
   console.log(req);
   res.json({ message: "data received", data: req.body });
 });
-//GET All JOBS
-app.get("/api/v1/jobs", (req, res) => {
-  res.status(200).json({ jobs });
-});
-//Create JOB
-app.post("/api/v1/jobs", (req, res) => {
-  const { company, position } = req.body;
-  if (!company || !position) {
-    return res.status(400).json({ msg: "please provide company and position" });
-  }
-  const id = nanoid(10);
-  //console.log(id)
-  const job = { id, company, position };
-  jobs.push(job);
-  res.status(200).json({ job });
+
+app.use("/api/v1/jobs", jobRouter);
+
+app.use("*", (req, res) => {
+  res.status(404).json({ msg: "not found" });
 });
 
-// GET SINGLE JOB
-app.get("/api/v1/jobs/:id", (req, res) => {
-  const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-  if (!job) {
-    return res.status(404).json({ msg: `no job with id ${id}` });
-  }
-  res.status(200).json({ job });
-});
-
-// EDIT JOB
-app.patch("/api/v1/jobs/:id", (req, res) => {
-  const { company, position } = req.body;
-  if (!company || !position) {
-    return res.status(400).json({ msg: "please provide company and position" });
-  }
-  const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-  if (!job) {
-    return res.status(404).json({ msg: `no job with id #{id}` });
-  }
-  job.company = company;
-  job.position = position;
-
-  res.status(200).json({ msg: `job modified`, job });
+app.use((err, req, res, next) => {
+  console(err);
+  res.status(500).json({ msg: "something went wrong" });
 });
 
 const port = process.env.PORT || 5100;
-
-app.listen(port, () => {
-  console.log(`server running on PORT ${port}...`);
-});
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+  app.listen(port, () => {
+    console.log(`server running on PORT ${port}...`);
+  });
+} catch (error) {
+  console.log(error);
+  process.exist(1);
+}
