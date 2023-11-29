6053b42cf79c05f252867a3dc118d68e7af6d7d0
Nazar <nazarx095@gmail.com>
2023-11-29 21:23:42 +0200

    register user complete

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 import "./index.css";
-
-fetch("/api/v1/test")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <ToastContainer position="top-center" />
   </React.StrictMode>
 );
