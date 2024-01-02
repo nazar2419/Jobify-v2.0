a3d40a905e054ea58265831b7af6c408d35fac49
Nazar <nazarx095@gmail.com>
2024-01-02 21:37:38 +0200

    pagenation complete

--- client/src/components/JobContainer.jsx
+++ client/src/components/JobContainer.jsx
@@ -1,10 +1,11 @@
 import Job from "./Job";
 import Wrapper from "../assets/wrappers/JobsContainer";
 import { useAllJobsContext } from "../pages/AllJobs";
-
+import PageBtnContainer from "./PageBtnContainer";
 const JobContainer = () => {
   const { data } = useAllJobsContext();
-  const { jobs } = data;
+
+  const { jobs, totalJobs, numOfPages } = data;
   if (jobs.length === 0) {
     return (
       <Wrapper>
@@ -14,11 +15,15 @@ const JobContainer = () => {
   }
   return (
     <Wrapper>
+      <h5>
+        {totalJobs} job{jobs.length > 1 && "s"} found
+      </h5>
       <div className="jobs">
         {jobs.map((job) => {
           return <Job key={job._id} {...job}></Job>;
         })}
       </div>
+      {numOfPages > 1 && <PageBtnContainer />}
     </Wrapper>
   );
 };
