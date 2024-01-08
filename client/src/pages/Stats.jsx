d62a46df57f10e7d5cc47d998976e1effc531850
Nazar <nazarx095@gmail.com>
2024-01-08 15:15:31 +0200

    react query - current user

--- client/src/pages/Stats.jsx
+++ client/src/pages/Stats.jsx
@@ -1,15 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
 import { ChartsContainer, StatsContainer } from "../components";
 import customFetch from "../utils/customFetch";
 import { useLoaderData } from "react-router-dom";
 
-export const loader = async () => {
-  const response = await customFetch.get("/jobs/stats");
-  return response.data;
+const statsQuery = {
+  queryKey: ["stats"],
+  queryFn: async () => {
+    const response = await customFetch.get("/jobs/stats");
+    return response.data;
+  },
+};
+
+export const loader = (queryClient) => async () => {
+  const data = await queryClient.ensureQueryData(statsQuery);
+  return data;
 };
 
 const Stats = () => {
-  const { defaultStats, monthlyApplications } = useLoaderData();
-  console.log(defaultStats);
+  // const { defaultStats, monthlyApplications } = useLoaderData();
+  const { data } = useQuery(statsQuery);
+  const { defaultStats, monthlyApplications } = data;
+
   return (
     <>
       <StatsContainer defaultStats={defaultStats} />
